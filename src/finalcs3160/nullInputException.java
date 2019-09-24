8431b8921bba845a923c449135be940c73ece4fe
Everett Johnson <elj10@q.com>
2019-09-24 09:00:53 -0600

    First Commit

---
+++ src/finalcs3160/nullInputException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalcs3160;
+
+public class nullInputException extends Exception{
+    
+    public nullInputException (String error){
+        System.out.println(error);
+    }
+    
+}
